d94311dd1f48149e68495e775a1cc57aca0dd067
zkalan <zkalan@qq.com>
2017-05-14 13:55:20 +0800

    初始化项目

---
+++ src/com/lexical/Main.java
@@ -0,0 +1,15 @@
+package com.lexical;
+/**
+ * @version 1.0.0
+ * @author zk
+ * 2017Äê5ÔÂ14ÈÕ 13:50
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
